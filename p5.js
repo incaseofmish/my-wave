5c72d5af048b95df076196b43e8eff7ef69f8cdc
incaseofmish <kolesoriginalmkm@gmail.com>
2026-03-23 23:26:07 +0300

    Update p5.js

--- p5.js
+++ p5.js
@@ -4,14 +4,20 @@ function setup() {
 
 function draw() {
   background(10, 15, 30);
-  let gap = width < 500 ? 15 : 25; 
   noStroke();
+  
+  // Определяем плотность сетки в зависимости от ширины экрана
+  let gap = (width < 600) ? 15 : 30; 
   
   for (let x = gap / 2; x < width; x += gap) {
     for (let y = gap / 2; y < height; y += gap) {
       let d = dist(mouseX, mouseY, x, y);
-      let size = map(d, 0, 200, gap * 1.5, 2, true);
-      fill(0, 180, 255, map(d, 0, 400, 255, 50));
+      
+      // На мобилках радиус реакции меньше, на компе больше
+      let radius = (width < 600) ? 150 : 300;
+      let size = map(d, 0, radius, gap * 1.5, 2, true);
+      
+      fill(0, 180, 255, map(d, 0, radius, 255, 50));
       ellipse(x, y, size, size);
     }
   }
