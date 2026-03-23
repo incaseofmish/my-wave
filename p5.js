7650a2745023f6f622866ddbae158ee91d903826
incaseofmish <kolesoriginalmkm@gmail.com>
2026-03-23 23:38:56 +0300

    Update p5.js

--- p5.js
+++ p5.js
@@ -7,13 +7,17 @@ function draw() {
   noStroke();
   
   let isMobile = width < 600;
-  let gap = isMobile ? 15 : 30;
+  let gap = isMobile ? 20 : 30;
   let sensitivity = isMobile ? 150 : 300;
+
+  // Если мышка еще не двигалась, ставим точку в центр экрана
+  let mx = mouseX || width / 2;
+  let my = mouseY || height / 2;
   
   for (let x = gap / 2; x < width; x += gap) {
     for (let y = gap / 2; y < height; y += gap) {
-      let d = dist(mouseX, mouseY, x, y);
-      let size = map(d, 0, sensitivity, gap * 1.6, 2, true);
+      let d = dist(mx, my, x, y);
+      let size = map(d, 0, sensitivity, gap * 1.5, 2, true);
       
       fill(0, 180, 255, map(d, 0, sensitivity, 255, 50));
       ellipse(x, y, size, size);
